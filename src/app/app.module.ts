014ac43a04eadf70385e8547d9d0b9058c17b0bc
Floor Bloemers <floorbloemers1@gmail.com>
2023-12-07 14:29:31 +0100

    added components and folders 'shared' and 'models'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './header/header.component';
+import { AppDropdownDirective } from './shared/app.dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent,
+    ProductListComponent,
+    HeaderComponent,
+    AppDropdownDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/app.dropdown.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAppDropdown]'
+})
+export class AppDropdownDirective {
+
+  constructor() { }
+
+}
